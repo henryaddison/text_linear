1ce86b70c7d1aab84bfb5159428543e869100e9c
Henry Addison <henry@dressipi.com>
2013-02-08 12:15:29 +0000

    get initial (pointless) spec to pass for Travis

---
+++ lib/text-linear.rb
@@ -0,0 +1,3 @@
+module TextLinear
+
+end
--- spec/text-linear_spec.rb
+++ spec/text-linear_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "TextLinear" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it "is a module" do
+    TextLinear.class == Module
   end
 end
