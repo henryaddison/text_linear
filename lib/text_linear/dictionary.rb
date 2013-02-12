e238ebba64fc4279e4743d1879219db7664b63b7
Henry Addison <henry@dressipi.com>
2013-02-12 16:32:23 +0000

    methods for creating a dictionary

--- lib/text_linear/dictionary.rb
+++ lib/text_linear/dictionary.rb
@@ -1,5 +1,25 @@
 module TextLinear
   class Dictionary
-    
+    attr_reader :words, :filepath
+
+    def initialize fp
+      @words = {}
+      @filepath = fp
+    end
+
+    def << word
+      words[word] = nil unless words.has_key?(word)
+    end
+
+    def save
+      index = 0
+      File.open(@filepath, 'w+') do |f|
+        words.keys.each do |w|
+          f.puts w
+          words[w] = index
+          index+=1
+        end
+      end
+    end
   end
 end
--- spec/text_linear/dictionary_spec.rb
+++ spec/text_linear/dictionary_spec.rb
@@ -1,8 +1,44 @@
 require 'spec_helper'
+
 describe TextLinear::Dictionary do
+  let(:filepath) { File.join(File.dirname(__FILE__), '..', '..', 'tmp', 'dictionaries', 'text.dictionary') }
+  before(:all) do
+    FileUtils.mkdir_p File.dirname(filepath)
+  end
+  
+  subject { TextLinear::Dictionary.new filepath}
+
   describe "#new" do
-    it "doesn't fail" do
-      TextLinear::Dictionary.new
+    it "has an empty bag of words" do
+      subject.words.should == {}
+    end
+
+    it 'should have a filepath' do
+      subject.filepath.should == filepath
+    end
+  end
+
+  describe "#<<" do
+    it 'adds a word' do
+      subject << "word"
+      subject.words.should have_key("word")
+    end
+  end
+
+  describe "#save" do
+    before(:each) do
+      subject << "word1"
+      subject << "word2"
+      subject.save
+    end
+
+    it 'assigns integers to words' do
+      subject.words["word1"].should == 0
+      subject.words["word2"].should == 1
+    end
+
+    it 'writes a file' do
+      File.read(filepath).should == "word1\nword2\n"
     end
   end
 end
